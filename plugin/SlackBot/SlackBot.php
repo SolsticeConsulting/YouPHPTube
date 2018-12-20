7419103acc4107ac12ca1f2f5aee8830599ea164
Will Hindenburg <WillH1983@yahoo.com>
2018-12-20 00:26:20 -0600

    Working on getting all subscribers
    Created JSON string from usersSubscribed array

--- plugin/SlackBot/SlackBot.php
+++ plugin/SlackBot/SlackBot.php
@@ -64,7 +64,7 @@ class SlackBot extends PluginAbstract
         $videoDescription = $video->getDescription();
         $token            = $o->bot_user_oauth_access_token;
         $slackChannel     = $o->channel_id;
-        $paylod->text     = $username . " just uploaded a video\nVideo Name: " . $videoName . "\nVideo Link: " . $videoLink . "\nVideo Duration: " . $videoDuration . "\nSubscribers: " . $usersSubscribed;
+        $paylod->text     = $username . " just uploaded a video\nVideo Name: " . $videoName . "\nVideo Link: " . $videoLink . "\nVideo Duration: " . $videoDuration . "\nSubscribers: " . json_encode($usersSubscribed);
         $paylod->channel  = $slackChannel;
         $message          = json_encode($paylod);
         $headers = array(
